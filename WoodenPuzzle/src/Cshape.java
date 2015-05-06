b41416ebc7bc171ac3a7fa1dd9b608c03a500167
armsndo <emmanueljo1994@hotmail.com>
2015-05-05 19:04:59 -0700

    Fixed Out of bounds logic

--- WoodenPuzzle/src/Cshape.java
+++ WoodenPuzzle/src/Cshape.java
@@ -8,7 +8,7 @@ public class Cshape extends Shape {
 		blocks[1] = new Block(x, y + 1, this);
 	}
 
-	public boolean OutOfBounds(int x, int y) {
+	public boolean NotOutBounds(int x, int y) {
 		if (x < 0 || y < 0 || grid.height < 0 || grid.width < 0)
 			return false;
 		return true;
@@ -18,7 +18,7 @@ public class Cshape extends Shape {
 	public boolean MoveUp() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-		if (OutOfBounds(x, y - 1)
+		if (NotOutBounds(x, y - 1)
 				&& grid.grid[x][y - 1].parent.letter.equals("E")
 				&& grid.grid[x + 1][y - 1].parent.letter.equals("E")) {
 			Eshape tmp = (Eshape) grid.grid[x][y - 1].parent;
@@ -40,7 +40,7 @@ public class Cshape extends Shape {
 	public boolean MoveDown() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-		if (OutOfBounds(x, y + 1)
+		if (NotOutBounds(x, y + 1)
 				&& grid.grid[x][y + 1].parent.letter.equals("E")
 				&& grid.grid[x + 1][y + 1].parent.letter.equals("E")) {
 			Eshape tmp = (Eshape) grid.grid[x][y + 1].parent;
@@ -62,7 +62,7 @@ public class Cshape extends Shape {
 	public boolean MoveLeft() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-		if (OutOfBounds(x - 1, y)
+		if (NotOutBounds(x - 1, y)
 				&& grid.grid[x - 1][y].parent.letter.equals("E")) {
 			Eshape tmp = (Eshape) grid.grid[x - 1][y].parent;
 			blocks[0].x -= 1;
@@ -78,7 +78,7 @@ public class Cshape extends Shape {
 	public boolean MoveRight() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-		if (OutOfBounds(x + 1, y)
+		if (NotOutBounds(x + 1, y)
 				&& grid.grid[x + 1][y].parent.letter.equals("E")) {
 			Eshape tmp = (Eshape) grid.grid[x + 1][y].parent;
 			blocks[0].x += 1;
