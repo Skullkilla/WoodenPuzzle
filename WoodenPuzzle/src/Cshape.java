c667c7b4d2e684f98d761853036bd7229071184e
armsndo <emmanueljo1994@hotmail.com>
2015-05-06 22:37:59 -0700

    Fixed C

--- WoodenPuzzle/src/Cshape.java
+++ WoodenPuzzle/src/Cshape.java
@@ -8,7 +8,7 @@ public class Cshape extends Shape {
 		blocks[1] = new Block(x, y + 1, this);
 	}
 
-	public boolean NotOutBounds(int x, int y) {
+	public boolean OutOfBounds(int x, int y) {
 		if (x < 0 || y < 0 || grid.height < 0 || grid.width < 0)
 			return false;
 		return true;
@@ -18,14 +18,8 @@ public class Cshape extends Shape {
 	public boolean MoveUp() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-<<<<<<< HEAD
 		if (OutOfBounds(x, y - 1)
 				&& grid.grid[x][y - 1].parent.letter.equals("E")) {
-=======
-		if (NotOutBounds(x, y - 1)
-				&& grid.grid[x][y - 1].parent.letter.equals("E")
-				&& grid.grid[x + 1][y - 1].parent.letter.equals("E")) {
->>>>>>> origin/master
 			Eshape tmp = (Eshape) grid.grid[x][y - 1].parent;
 			blocks[0].y -= 1;
 			grid.grid[x][y - 1] = blocks[0];
@@ -40,14 +34,8 @@ public class Cshape extends Shape {
 	public boolean MoveDown() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-<<<<<<< HEAD
 		if (OutOfBounds(x, y + 1)
 				&& grid.grid[x][y + 1].parent.letter.equals("E")) {
-=======
-		if (NotOutBounds(x, y + 1)
-				&& grid.grid[x][y + 1].parent.letter.equals("E")
-				&& grid.grid[x + 1][y + 1].parent.letter.equals("E")) {
->>>>>>> origin/master
 			Eshape tmp = (Eshape) grid.grid[x][y + 1].parent;
 			blocks[0].y += 1;
 			grid.grid[x][y + 1] = blocks[0];
@@ -62,14 +50,9 @@ public class Cshape extends Shape {
 	public boolean MoveLeft() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-<<<<<<< HEAD
 		if (OutOfBounds(x - 1, y)
 				&& grid.grid[x - 1][y].parent.letter.equals("E")
 				&& grid.grid[x - 1][y + 1].parent.letter.equals("E")) {
-=======
-		if (NotOutBounds(x - 1, y)
-				&& grid.grid[x - 1][y].parent.letter.equals("E")) {
->>>>>>> origin/master
 			Eshape tmp = (Eshape) grid.grid[x - 1][y].parent;
 			Eshape tmp2 = (Eshape) grid.grid[x - 1][y + 1].parent;
 			blocks[0].x -= 1;
@@ -89,14 +72,9 @@ public class Cshape extends Shape {
 	public boolean MoveRight() {
 		int x = blocks[0].x;
 		int y = blocks[0].y;
-<<<<<<< HEAD
 		if (OutOfBounds(x + 1, y)
 				&& grid.grid[x + 1][y].parent.letter.equals("E")
 				&& grid.grid[x + 1][y + 1].parent.letter.equals("E")) {
-=======
-		if (NotOutBounds(x + 1, y)
-				&& grid.grid[x + 1][y].parent.letter.equals("E")) {
->>>>>>> origin/master
 			Eshape tmp = (Eshape) grid.grid[x + 1][y].parent;
 			Eshape tmp2 = (Eshape) grid.grid[x + 1][y + 1].parent;
 			blocks[0].x += 1;
@@ -113,3 +91,5 @@ public class Cshape extends Shape {
 
 	}
 }
+
+    
